3ac1cde11488ec6499527701b4ed05fd635da623
Skverkl <beskrovnyiigor5@gmail.com>
2024-05-30 20:41:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function open1() {
+    window.location.href = "1.html";
+}
+function open2() {
+    window.location.href = "2.html";
+}
+function open3() {
+    window.location.href = "3.html";
+}
+function open4() {
+    window.location.href = "4.html";
+}
